08df57e36228d726447e08a9cb5f666935e985ea
Your Name <surinamio@gmail.com>
2023-07-19 18:49:10 -0400

    changed App.js component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React from 'react';
-//import ReactDOM from 'react-dom';
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navigation from './Navigation';
-import Home from './Home';
-import About from './About';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-// import './index.css';
-// import './styles.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="container">
-          <Navigation />
-          <Routes>
-            <Route path="/" element={Home} />
-            <Route path="/about" element={About} />
-            <Route path="/portfolio" element={Portfolio} />
-            <Route path="/contact" element={Contact} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+      <Routes>
+        <Route path="/home" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
