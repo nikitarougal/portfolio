ea6f3c0b18ee721780c2eebab783952668415db0
Your Name <surinamio@gmail.com>
2023-07-20 09:52:53 -0400

    add Footer.js componet

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="py-4 text-center bg-primary text-white ">
+      {' '}
+      Copyright &copy; 2023 Nik Rougal. All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
