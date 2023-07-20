1aec96163502b4f13a9c763d0931e454ec7262a5
Your Name <surinamio@gmail.com>
2023-07-20 14:57:25 -0400

    add Home component and some changes to routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Hero />
-        <About />
-        <Portfolio />
-        <Contact />
-        <Footer />
-      </div>
-      <Routes>
-        <Route path="/home" element={<Hero />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Home />
+    </div>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header';
+import Hero from './Hero';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
